c548f303837ad37cd6ec0c9100da2b8cbbb73e5f
Charles Sandidge <sandidgec@gmail.com>
2016-10-13 09:00:37 -0600

    added bootstrap link + more
    added some links, made sure sh** is working

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php require_once("headutils.php");?>
+    <?php require_once("lib/headutils.php");?>
   </head>
   <body>
+    <div class="jumbotron">
+        Hello Jumbotron
+    </div>
     <h1>Hello Welcome TEAM CK</h>
+
   </body>
 </html>
--- lib/headutils.php
+++ lib/headutils.php
@@ -5,15 +5,15 @@
   <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 
-  <!-- Bootstrap - Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-    integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-    crossorigin="anonymous">
 
 <link href='https://fonts.googleapis.com/css?family=Montserrat|Raleway:400,300' rel='stylesheet' type='text/css'>
 
 <!-- Custom CSS -->
-<link rel="stylesheet" href="css/style.css">
+
+<link rel="stylesheet" href="lib/css/bootstrap.css">
+<link rel="stylesheet" href="lib/css/about.css">
+<link rel="stylesheet" href="lib/css/index.css">
+
 
 <!-- Font Awesome -->
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
@@ -25,5 +25,3 @@
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
       integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
       crossorigin="anonymous"></script>
-
-  
